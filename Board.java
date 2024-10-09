bd84e7ba055df7a876e14fd062e10b83a598e2f5
Elwood <elwoodwjz@outlook.com>
2024-10-09 17:11:40 +0100

    add initialiseBoard method which will create a board for gaming

--- Board.java
+++ Board.java
@@ -1,9 +1,21 @@
 public class Board {
     private int rows;
     private int columns;
-    private Square tables;
+    private Square[][] tables;
 
     public void Board(int rows, int columns){
-
+        this.rows = rows;
+        this.columns = columns;
+        this.tables =  new Square[rows][columns];
     }
+
+    //Print the board table on terminal.
+    private void initialiseBoard(){
+        for (int i = 0; i < rows; ++i){
+            for (int j = 0; j < columns; ++j){
+                tables[i][j] = new Square(i,j);
+            }
+        }
+    }
+
 }
